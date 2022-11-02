5facbc2267272cb98fc89e1024452290e406c84e
Miguel Rodriguez <miguel.rodriguez@whitecloak.com>
2022-11-02 18:16:26 +0800

    Add close confirmation and buffer to check logs

--- time-in.py
+++ time-in.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
+
 import os 
 from dotenv import load_dotenv
 load_dotenv()
@@ -20,8 +22,9 @@ def checkIsLoggedIn():
     return firstRowText == 'IN'
 
 def timeinOrTimeout():
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, "small-image")))
-    driver.find_element(By.CLASS_NAME, "small-image").click()
+    clockClassname = "small-image"
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, clockClassname)))
+    driver.find_element(By.CLASS_NAME, clockClassname).click()
 
     isLoggedIn = checkIsLoggedIn()
     if isLoggedIn: 
@@ -29,15 +32,31 @@ def timeinOrTimeout():
     else: 
         driver.find_element(By.XPATH, '//li[@data-bind="click: webBundyLogIn"]').click()
 
-# def confirmPopup():   
-#     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[@data-bb-handler='danger']")))
-#     driver.find_element(By.XPATH, "//button[@data-bb-handler='success']").click()
+def confirmPopUp():   
+    successButtonXPath = "//button[@data-bb-handler='success']"
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, successButtonXPath)))
+    driver.find_element(By.XPATH, successButtonXPath).click()
+
+def closeConfirmationPopUp():
+    okButtonXPath = "//button[@data-bb-handler='ok']"
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, okButtonXPath)))
+    driver.find_element(By.XPATH, okButtonXPath).click()
+
+def reloadPage(): 
+    driver.refresh()
+
+def closeAndQuitBrowser(): 
+    # 5 seconds to check if login/logout is logged on dashboard
+    time.sleep(5)
+    driver.close()
+    driver.quit()
 
 try: 
     login()
     timeinOrTimeout()
-    # confirmPopup()
-    print("Logged in successfully")
+    confirmPopUp()
+    closeConfirmationPopUp()
+    reloadPage()
+    closeAndQuitBrowser()
 except: 
     print("Something went wrong")
-
